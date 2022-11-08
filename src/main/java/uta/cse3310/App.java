4e281aad3046033b318cc21af4ce81a72a3d86f6
mWhite8 <malachischool27@gmail.com>
2022-11-07 22:54:00 -0600

    server stuff

---
+++ src/main/java/uta/cse3310/App.java
@@ -0,0 +1,107 @@
+package uta.cse3310;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.util.Collections;
+
+import org.java_websocket.WebSocket;
+import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class App extends WebSocketServer{
+    Vector<BlackJack> ActiveGames = new Vector<BlackJack>();
+    int GameId = 1;
+
+    public App(int port){
+        super(new InetSocketAddress(port));
+    }
+
+    public App(InetSocketAddress add){
+        super(add);
+    }
+
+    public App(int port, Draft_6455 draft) {
+        super(new InetSocketAddress(port), Collections.<Draft>singletonList(draft));
+      }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        System.out.println(conn + " has closed");
+        
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        ex.printStackTrace();
+        if (conn != null) {
+          // some errors like port binding failed may not be assignable to a specific
+          // websocket
+        }
+      }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        System.out.println(conn + ": " + message);
+        // Bring in the data from the webpage
+        // A UserEvent is all that is allowed at this point
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        //UserEvent U = gson.fromJson(message, UserEvent.class);
+        System.out.println(U.Button);
+
+        // Get our Game Object
+        
+
+        // send out the game state every time
+        // to everyone
+        String jsonString;
+        jsonString = gson.toJson(G);
+
+        System.out.println(jsonString);
+        broadcast(jsonString);
+    }
+
+    public void onMessage(WebSocket conn, ByteBuffer message) {
+        System.out.println(conn + ": " + message);
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        System.out.println(conn.getRemoteSocketAddress().getAddress().getHostAddress() + " connected");
+        
+    }
+
+    @Override
+    public void onStart() {
+        System.out.println("Server started!");
+        setConnectionLostTimeout(0);
+    }
+
+    public static void main(String[] args) {
+
+        // Set up the http server
+        int port = 9083;
+        HttpServer H = new HttpServer(port, "./html");
+        H.start();
+        System.out.println("http Server started on port:" + port);
+    
+        // create and start the websocket server
+    
+        port = 9883;
+        App A = new App(port);
+        A.start();
+        System.out.println("websocket Server started on port: " + port);
+    
+      }
+}
