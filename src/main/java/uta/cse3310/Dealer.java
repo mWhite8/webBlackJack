8b1344061a738c74e02637276e25268e920567c7
rtygya <rxg3756@mavs.uta.edu>
2022-11-09 18:37:01 -0600

    first commit

--- src/main/java/uta/cse3310/Dealer.java
+++ src/main/java/uta/cse3310/Dealer.java
@@ -7,8 +7,20 @@ public class Dealer
 
     public double tenProbability()
     {
-        //placeholder until functionality is added
         double prob = 0;
+        // Reety
+        int card_count = dealerHand.cards.size();
+        int num_of_tens = 0;
+        
+        for (int i = 0; i < card_count; i++)
+        {
+            if (dealerHand.cards.get(i).getValue() == 10)
+            {
+                num_of_tens++;
+            }
+        }
+
+        prob = (double) num_of_tens/card_count;
         return prob;
     }
 
