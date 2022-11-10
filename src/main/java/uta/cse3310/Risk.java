737724e0f44634e56eea44b4b31498bde9c86982
Pedro <pxs3662@mavs.uta.edu>
2022-11-09 23:47:15 -0600

    adding new class

---
+++ src/main/java/uta/cse3310/Risk.java
@@ -0,0 +1,41 @@
+
+// Pedro Santana - adding random class to handle all of the risk levels for players and bots in one place
+
+
+/* 
+
+package uta.cse3310;
+import java.util.Random;
+
+public class Risk {
+
+    
+    Random r = new Random();
+    int riskChoice = 0;
+    int risk = r.nextInt();
+
+    public int riskLevel(String name){
+
+        switch(name){
+            case "Minimal":
+            riskChoice= 1;
+            case "Normal":
+            riskChoice = 2;
+            case "High":
+            riskChoice = 3;
+            case "Counter":
+            riskChoice = 4;
+            default:
+            return 0;
+        }
+
+        return riskChoice;
+        
+    }
+    
+}
+
+
+*/
+
+//Pedro Santana
