a5d4fc2261d55910b0d7995333e2cf6c1c81f159
pxs3662 <72286391+pxs3662@users.noreply.github.com>
2022-11-17 00:00:38 -0600

    Create PointCount.java

---
+++ src/main/java/uta/cse3310/PointCount.java
@@ -0,0 +1,13 @@
+package uta.cse3310;
+
+import java.util.ArrayList;
+
+public class PointCount 
+{
+    /* 
+    public void Winner(ArrayList<Player> listOfPlayers, ArrayList<Card> deck)
+    {
+
+    }
+    */
+}
